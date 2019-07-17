60cff7abeb025906468dceb9f81453347e284c71
Jules Lee <jackconsolidated@gmail.com>
2019-07-17 21:07:44 +0800

    Initial Commit

---
+++ Seefood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seefood
+//
+//  Created by Jules Lee on 17/07/2019.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
